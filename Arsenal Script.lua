2a7c4dd6de89daf220885e10a5cf27201db64b0b
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2024-05-02 22:29:58 +0300

    Update Arsenal Script.lua

--- Arsenal Script.lua
+++ Arsenal Script.lua
@@ -127,33 +127,7 @@ for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
        v.Value = 20
    end
 end
-
-
-function getplrsname() for i,v in pairs(game:GetChildren()) do if v.ClassName == "Players" then return v.Name end end end local players = getplrsname() local plr = game[players].LocalPlayer coroutine.resume(coroutine.create(function() while wait(1) do coroutine.resume(coroutine.create(function() for _,v in pairs(game[players]:GetPlayers()) do if v.Name ~= plr.Name and v.Character then v.Character.RightUpperLeg.CanCollide = false v.Character.RightUpperLeg.Transparency = 75 v.Character.RightUpperLeg.Size = Vector3.new(21,21,21) v.Character.LeftUpperLeg.CanCollide = false v.Character.LeftUpperLeg.Transparency = 75 v.Character.LeftUpperLeg.Size = Vector3.new(21,21,21) v.Character.HeadHB.CanCollide = false v.Character.HeadHB.Transparency = 75 v.Character.HeadHB.Size = Vector3.new(21,21,21) v.Character.HumanoidRootPart.CanCollide = false v.Character.HumanoidRootPart.Transparency = 75 v.Character.HumanoidRootPart.Size = Vector3.new(21,21,21) end end end)) end end))
  
---Made by Andrheyplayz_officalyt
-local gui = Instance.new("BillboardGui");
-gui.Name = "";
-gui.AlwaysOnTop = true;
-gui.LightInfluence = 0;
-gui.Size = UDim2.new(1.75, 0, 1.75, 0);
-local frame = Instance.new("Frame", gui);
-frame.BackgroundColor3 = Color3.fromRGB(170, 0, 0);
-frame.Size = UDim2.new(1, 0, 1, 0);
-frame.BorderSizePixel = 4;
-frame.BorderColor3 = Color3.fromRGB(0, 0, 0);
-local gi = gui:Clone();
-local body = frame:Clone();
-body.Parent = gi;
-body.BackgroundColor3 = Color3.fromRGB(0, 170, 170);
- 
-for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-    if v.Name ~= game:GetService("Players").LocalPlayer.Name and v.Character and v.Character:FindFirstChild("Head") then
-        gui:Clone().Parent = v.Character.Head;
-    end
-end
- 
-local c = 1 function zigzag(X)  return math.acos(math.cos(X * math.pi)) / math.pi end game:GetService("RunService").RenderStepped:Connect(function()  if game.Workspace.Camera:FindFirstChild('Arms') then   for i,v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do    if v.ClassName == 'MeshPart' then      v.Color = Color3.fromHSV(zigzag(c),1,1)     c = c + .0001    end   end  end end)
 net = true -- if false = do nothing
 notify = false -- set this to false if u don't want to see notiflication 
 
